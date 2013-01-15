7fbbf238ae86674b7a8f60cdd1415bc4abc2d76a
Rick Waldron <waldron.rick@gmail.com>
2013-01-15 17:50:34 -0500

    Initial/functional reststop feature support
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- app/modules/reststop.js
+++ app/modules/reststop.js
@@ -1,39 +1,47 @@
 define([
-    "app"
+    "app",
+    "dom",
+    "moment",
 
-], function( App ) {
+    "json!data/reststops.json"
 
-    var Reststop;
+], function( App, DOM, moment, reststops ) {
+
+    var Reststop,
+        TWITTER_USER = "jquerybot";
 
     Reststop = App.module();
 
+    Reststop.templates = {
+        post: _.template("<div class='post'><div class='body'><%=text%></div><div class='name'><%=from_user_name%></div>|<div class='date'><%=created_at%></div></div>")
+    };
+
+    // TODO:
+    //
+    // Refactor this and roadById into a single
+    // abstract lookup function.
+    function restById( id ) {
+        var i = 0,
+            length = reststops.length;
+
+        for ( ; i < length; i++ ) {
+            if ( reststops[ i ].id === id ) {
+                return reststops[ i ];
+            }
+        }
+        return null;
+    }
+
     Reststop.Model = Backbone.Model.extend({
         defaults: {
-            isAvailable: false,
-
-            // |profiles| is an object of data pro
+            id: null,
             profiles: null
         },
-        // ,
-        // url: function() {
-            // return "http://alpha.zeega.org/api/items/" + this.id;
-        // },
-        // parse: function( obj ) {
-        //     var data = obj.items[ 0 ];
 
-        //     // API requests are coming up empty handed :(
-        //     if ( !data ) {
-        //         console.warn(
-        //             "Zeega API failed to respond with project data. Requested from: ", this.url()
-        //         );
-        //     }
-
-
-        //     return data;
-        // },
-        initialize: function() {
-            // Push all new Reststop.Model instances into
-            // the Reststop.Items collection
+        initialize: function( reststop ) {
+            this.set(
+                _.extend( reststop, restById( reststop.id ) )
+            );
             Reststop.Items.add( this );
         }
     });
@@ -42,11 +50,7 @@ define([
         model: Reststop.Model
     });
 
-    // Keep
     Reststop.Items = new Reststop.Collection();
-
-
-    // http://search.twitter.com/search.json?q=@zeega&rpp=100&result_type=recent
 
     Reststop.Views.Item = Backbone.View.extend({
         manage: true,
@@ -60,17 +64,94 @@ define([
         },
 
         initialize: function( config ) {
-            //
-        },
-
-        beforeRender: function() {
-
-            console.log( "Reststop.Views.Item: beforeRender" );
+            this.model = Reststop.Items.get( config.id );
         },
 
         afterRender: function() {
+            $.ajax({
+                type: "GET",
+                url: "http://search.twitter.com/search.json?q=" + TWITTER_USER + "&rpp=100&result_type=recent&callback=?",
+                dataType: "jsonp",
+                success: function( data ) {
+                    console.log( data );
+                    data.results.filter( isValid ).slice(0, 25).forEach( draw );
+
+                    console.log( data.results.filter( isValid ) );
+                }
+            });
         }
     });
+
+    // TEMPORARY IMPLEMENTATION
+    //
+    // TODO: Refactor this to a constructor
+    //
+    //
+    function isValid( tweet ) {
+        return tweet.from_user.toLowerCase() === TWITTER_USER &&
+            [ "RT", "@" ].every(function( prefix ) {
+                return tweet.text.indexOf( prefix ) === -1;
+            });
+    }
+    function draw( tweet ) {
+        var $div, $reststop, color, size, coords, style;
+
+        $reststop = $("#reinvention-reststop");
+        $div = $("<div>");
+        color = "magenta";
+        size = 50;
+        coords = {
+            x: ( Math.random() * ( (DOM.doc.width() - 300) - 20 ) + 20 ).toFixed(),
+            y: ( Math.random() * ( (DOM.doc.height() - 150) - 110 ) + 110 ).toFixed()
+        };
+        style = {
+            position: "absolute",
+            left: coords.x + "px",
+            top: coords.y + "px",
+            display: "none",
+            width: 50 + "px",
+            height: size + "px",
+            backgroundColor: color,
+            opacity: 0.25,
+            zIndex: 995
+        };
+
+        $div.css( style );
+        $div.appendTo( $reststop ).fadeIn( 100 ).delay( 1000 ).animate({
+            opacity: 0.25
+        }, 400 );
+
+        $div.on("mouseover mouseleave", function( event ) {
+
+            if ( event.type === "mouseover" ) {
+
+                $(this).css({ zIndex: 999 }).animate({
+                    width: "300px",
+                    height: "150px",
+                    opacity: 1
+                }, 200, function() {
+
+                    // TODO: Refactor to use template
+                    $(this).html(
+                        Reststop.templates.post(
+                            Abstract.merge({}, tweet, {
+
+                                created_at: moment( new Date( tweet.created_at ) ).from(
+                                    moment( new Date() )
+                                )
+
+                            })
+                        )
+                    );
+                });
+
+            } else {
+                $(this).empty().animate( style, 400, function() {
+                    $(this).empty();
+                });
+            }
+        });
+    }
 
 
 
