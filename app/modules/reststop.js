a577c69990d993be81c03f7489d7570758878732
Rick Waldron <waldron.rick@gmail.com>
2013-01-16 14:17:13 -0500

    Implement reststop behavious
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- app/modules/reststop.js
+++ app/modules/reststop.js
@@ -3,17 +3,19 @@ define([
     "dom",
     "moment",
 
-    "json!data/reststops.json"
+    "json!data/reststops.json",
+    "text!templates/reststop/answer.html"
 
-], function( App, DOM, moment, reststops ) {
+], function( App, DOM, moment, reststops, answer ) {
 
     var Reststop,
-        TWITTER_USER = "jquerybot";
+        priv = new WeakMap(),
+        TWITTER_USER = "rwaldron";
 
     Reststop = App.module();
 
     Reststop.templates = {
-        post: _.template("<div class='post'><div class='body'><%=text%></div><div class='name'><%=from_user_name%></div>|<div class='date'><%=created_at%></div></div>")
+        answer: _.template( answer )
     };
 
     // TODO:
@@ -42,6 +44,7 @@ define([
             this.set(
                 _.extend( reststop, restById( reststop.id ) )
             );
+            console.log( this );
             Reststop.Items.add( this );
         }
     });
@@ -64,95 +67,132 @@ define([
         },
 
         initialize: function( config ) {
+            console.log( "Reststop.Views.Item: initialize" );
             this.model = Reststop.Items.get( config.id );
         },
 
         afterRender: function() {
-            $.ajax({
-                type: "GET",
-                url: "http://search.twitter.com/search.json?q=" + TWITTER_USER + "&rpp=100&result_type=recent&callback=?",
-                dataType: "jsonp",
-                success: function( data ) {
-                    console.log( data );
-                    data.results.filter( isValid ).slice(0, 25).forEach( draw );
+            var id, interval;
 
-                    console.log( data.results.filter( isValid ) );
+            id = $("#reinvention-reststop").data("view");
+
+            // Request tweets every 20s
+            interval = setInterval(function() {
+                if ( $("#reinvention-reststop").data("view") !== id ) {
+                    clearInterval(interval);
+                } else {
+                    Answer.request();
                 }
-            });
+            }, 2e5);
+
+            // Initial request...
+            Answer.request();
         }
     });
 
-    // TEMPORARY IMPLEMENTATION
-    //
-    // TODO: Refactor this to a constructor
-    //
-    //
-    function isValid( tweet ) {
-        return tweet.from_user.toLowerCase() === TWITTER_USER &&
-            [ "RT", "@" ].every(function( prefix ) {
-                return tweet.text.indexOf( prefix ) === -1;
-            });
-    }
-    function draw( tweet ) {
-        var $div, $reststop, color, size, coords, style;
 
-        $reststop = $("#reinvention-reststop");
-        $div = $("<div>");
-        color = "magenta";
-        size = 50;
-        coords = {
+    function Answer( tweet ) {
+        // Add this tweet's id to the known set of valid ids.
+        // ... This will be used to prevent duplicates in the future.
+        Answer.isValid.knownIds.add( tweet.id );
+
+        //  TODO: make the resttop an arbitrary element from an arbtrary selector
+        this.$reststop = $("#reinvention-reststop");
+        this.$point = $("<div>");
+        this.coords = {
             x: ( Math.random() * ( (DOM.doc.width() - 300) - 20 ) + 20 ).toFixed(),
             y: ( Math.random() * ( (DOM.doc.height() - 150) - 110 ) + 110 ).toFixed()
         };
-        style = {
-            position: "absolute",
-            left: coords.x + "px",
-            top: coords.y + "px",
-            display: "none",
-            width: 50 + "px",
-            height: size + "px",
-            backgroundColor: color,
-            opacity: 0.25,
+        this.style = {
+            left: this.coords.x + "px",
+            top: this.coords.y + "px",
             zIndex: 995
         };
 
-        $div.css( style );
-        $div.appendTo( $reststop ).fadeIn( 100 ).delay( 1000 ).animate({
+        this.$point.css( this.style ).addClass( "reststop-point" );
+
+        this.$point.appendTo( this.$reststop ).delay( 1000 ).animate({
             opacity: 0.25
-        }, 400 );
+        }, 200 );
 
-        $div.on("mouseover mouseleave", function( event ) {
 
-            if ( event.type === "mouseover" ) {
+        this.created = moment(
+            new Date( tweet.created_at )
+        );
 
-                $(this).css({ zIndex: 999 }).animate({
-                    width: "300px",
-                    height: "150px",
-                    opacity: 1
-                }, 200, function() {
+        this.$answer = $(
+            $.parseHTML(
+                Reststop.templates.answer( tweet ).trim()
+            )
+        ).css( this.style );
 
-                    // TODO: Refactor to use template
-                    $(this).html(
-                        Reststop.templates.post(
-                            Abstract.merge({}, tweet, {
 
-                                created_at: moment( new Date( tweet.created_at ) ).from(
-                                    moment( new Date() )
-                                )
+        this.$point.on("mouseover mouseleave click", function( event ) {
 
-                            })
-                        )
-                    );
-                });
+            if ( event.type === "click" ) {
+                this.reveal();
 
             } else {
-                $(this).empty().animate( style, 400, function() {
-                    $(this).empty();
-                });
+                this.$point.show();
+
+                this.$point.finish().animate({
+                    opacity: event.type === "mouseover" ? 1 : 0.25
+                }, 200);
             }
-        });
+        }.bind(this));
+
+        // Save a private copy of the tweet data
+        priv.set( this, Abstract.assign({}, tweet) );
+
+        Answer.Rendered.push( this.$answer );
     }
 
+    Answer.prototype.reveal = function() {
+        // console.log( Answer.Rendered.length );
+        console.log( priv.get( this ) );
+
+        // Hide Other answers
+        Answer.Rendered.forEach(function( answer ) {
+            if ( answer !== this.$answer ) {
+                answer.fadeOut( 200 , function() {
+                    answer.remove();
+                });
+            }
+        }, this );
+
+        this.$answer.appendTo( this.$reststop ).fadeIn();
+
+        return this;
+    };
+
+    // Cache of all jQuery object
+    Answer.Rendered = [];
+
+
+    Answer.isValid = function( tweet ) {
+        return !Answer.isValid.knownIds.has( tweet.id );// &&
+        //     [ "RT", "@" ].every(function( prefix ) {
+        //         return tweet.text.indexOf( prefix ) === -1;
+        //     });
+    };
+
+    Answer.isValid.knownIds = new Set();
+
+    Answer.request = function() {
+        return $.ajax({
+            type: "GET",
+            url: "https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=" + TWITTER_USER + "&count=20&callback=?",
+            dataType: "jsonp",
+            success: function( data ) {
+                // console.log( data );
+                data.filter( Answer.isValid ).slice(0, 25).forEach( Answer.create );
+            }
+        });
+    };
+
+    Answer.create = function( tweet ) {
+        return new Answer( tweet );
+    };
 
 
     return Reststop;
