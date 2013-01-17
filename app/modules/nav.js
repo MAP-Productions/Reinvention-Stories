3dcc84b6b705fa9785a276fb15cbeef8f1197741
Rick Waldron <waldron.rick@gmail.com>
2013-01-17 17:48:27 -0500

    Clean up app/modules/nav.js
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- app/modules/nav.js
+++ app/modules/nav.js
@@ -1,15 +1,17 @@
 require([],
 function() {
 
-    var nav, isHidden, isLocked, last;
+    var $nav, isRolledup, isLocked, last;
 
     isLocked = false;
     last = 0;
 
     function mousemove( event ) {
+        event = event || { pageY: 0 };
+
         // Upvars declared in enclosing scope
-        nav = $("#reinvention-menu");
-        isHidden = nav.hasClass("rolledup");
+        $nav = $("#reinvention-menu");
+        isRolledup = $nav.hasClass("rolledup");
 
         if ( last > 100 && event.pageY <= 100 ) {
             isLocked = false;
@@ -20,22 +22,23 @@ function() {
         }
 
         if ( event.pageY > 100 ) {
-            if ( !isHidden ) {
+            if ( !isRolledup ) {
                 isLocked = true;
-                nav.addClass("rolledup");
-                nav.animate( {
-                    top: '-77px'
+                $nav.animate({
+                    top: "-77px"
                 }, 500, function() {
+                    $(this).addClass("rolledup");
                     isLocked = false;
                 });
             }
         } else {
-            if ( isHidden ) {
+            if ( isRolledup ) {
                 isLocked = true;
-                nav.removeClass("rolledup");
-                nav.animate( {
+
+                $nav.animate( {
                     top: 0
                 }, 500, function() {
+                    $(this).removeClass("rolledup");
                     isLocked = false;
                 });
             }
@@ -44,9 +47,12 @@ function() {
 
     $(document).on( "mousemove", mousemove );
 
-    _.delay( function() {
-        mousemove({ pageY: 99 });
-    }, 2000 );
+    // Trigger the mousemove with a fake event.pageY = 99 to hold the menu open.
+    // NOTE: this is note straight forward, because the the transition timer
+    // is buried in CSS
+    setTimeout( mousemove, 2000 );
+
+
 
     return {
         mousemove: mousemove
