5e2227b6d3bef0c8f7995d05218bc400461a7cb8
Rick Waldron <waldron.rick@gmail.com>
2013-01-16 20:57:11 -0500

    app/modules/intro.js
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

---
+++ app/modules/intro.js
@@ -0,0 +1,67 @@
+define([
+    "app"
+
+], function( App ) {
+
+    var Intro = App.module();
+
+    Intro.Model = Backbone.Model.extend({
+        defaults: {
+            id: 1,
+            video: "Intro"
+        }
+    });
+
+    Intro.Collection = Backbone.Collection.extend({
+        model: Intro.Model
+    });
+
+    Intro.Items = new Intro.Collection();
+
+    Intro.Views.Item = Backbone.View.extend({
+
+        template: "intro/item",
+
+        data: function() {
+            return {
+                model: this.model
+            };
+        },
+
+        initialize: function( config ) {
+            this.model = Intro.Items.get( config.id );
+            this.model.set({
+                view: this
+            });
+        },
+
+        beforeRender: function() {
+            console.log( "Intro.Views.Item: beforeRender" );
+        },
+
+        afterRender: function() {
+            console.log( "Intro.Views.Item: afterRender" );
+
+            var $pop = Popcorn("#reinvention-intro video");
+
+            function handler() {
+                App.router.go( "act", 1, "story", 1 );
+            }
+
+            $pop.code({
+                start: 8,
+                end: 12,
+                onStart: function() {
+                    this.on( "click", handler );
+                },
+                onEnd: function() {
+                    this.off( "click", handler );
+                }
+            });
+        }
+    });
+
+    Intro.Items.add({ id: 1 });
+
+    return Intro;
+});
