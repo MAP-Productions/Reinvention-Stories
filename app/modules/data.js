603968fdd021a45476233a6e52d349d251687675
Rick Waldron <waldron.rick@gmail.com>
2013-01-18 11:35:27 -0500

    Adds new app/modules/data.js; Data acts a broker for all required json data
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

---
+++ app/modules/data.js
@@ -0,0 +1,44 @@
+define([
+
+    "json!data/acts.json",
+    "json!data/stories.json",
+    "json!data/roads.json",
+    "json!data/reststops.json",
+
+], function( acts, stories, roads, reststops ) {
+    var priv, map, args;
+
+    priv = new WeakMap();
+    map = {};
+    args = [].slice.call( arguments );
+
+    [ "acts", "stories", "roads", "reststops" ].forEach(function( key, i ) {
+        map[ key ] = args[i];
+    });
+
+    function From( key ) {
+        priv.set( this, map[ key ] );
+    }
+
+    From.prototype.byId = function( id ) {
+        var data, k = -1;
+
+        data = priv.get( this );
+
+        while ( ++k < data.length ) {
+            if ( data[ k ].id === id ) {
+                return data[ k ];
+            }
+        }
+        return null;
+    };
+
+    return {
+        get: function( type ) {
+            return map[ type ];
+        },
+        from: function( plural ) {
+            return new From( plural );
+        }
+    };
+});
