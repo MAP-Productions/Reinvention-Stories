fd6774f4880b2971094f23062e339f131b6c1aee
Eric Demicco <edemicco@gmail.com>
2013-03-13 01:43:57 -0400

    Contribute cosmetics.

--- app/modules/contribute.js
+++ app/modules/contribute.js
@@ -34,14 +34,14 @@ define([
                 windowWidth = App.DOM.$body.width();
 
             // send the current section off to the left of the screen
-            current.removeClass( "active-section" ).animate( {
+            current.removeClass("active-section").animate( {
                 left: windowWidth * -1
             }, function() {
                 $(this).hide();
             } );
 
             // position the next section to the right of the viewport and make it visible
-            next.addClass( "active-section" ).css({
+            next.addClass("active-section").css({
                 left: windowWidth
             }).show();
 
@@ -60,14 +60,14 @@ define([
                 windowWidth = App.DOM.$body.width();
 
             // send the current section off to the right of the screen
-            current.removeClass( "active-section" ).animate( {
+            current.removeClass("active-section").animate( {
                 left: windowWidth
             }, function() {
                 $(this).hide();
             } );
 
             // position the next section to the left of the viewport and make it visible
-            prev.addClass( "active-section" ).css({
+            prev.addClass("active-section").css({
                 left: windowWidth * -1
             }).show();
 
@@ -80,19 +80,27 @@ define([
         },
 
         updateNavAndArrows: function() {
+            var index = this.$(".active-section").index(".contribute-section");
+
             // show or hide next arrow
-            if ( this.$(".active-section" ).next( ".contribute-section" ).length > 0 ) {
-                this.$(".next").show();
+            if ( this.$(".active-section").next(".contribute-section").length > 0 ) {
+                this.$(".next").fadeIn();
             } else {
-                this.$(".next").hide();
+                this.$(".next").fadeOut();
             }
 
             // show or hide previous arrow
-            if ( this.$(".active-section" ).prev( ".contribute-section" ).length > 0 ) {
-                this.$(".prev").show();
+            if ( this.$(".active-section").prev(".contribute-section").length > 0 ) {
+                this.$(".prev").fadeIn();
             } else {
-                this.$(".prev").hide();
+                this.$(".prev").fadeOut();
             }
+
+            // activate the correct section name in the list
+            this.$(".contribute-nav li").eq( index ).addClass("active")
+                .siblings().removeClass("active");
+
+
         }
 
     });
