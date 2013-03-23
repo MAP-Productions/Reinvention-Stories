04f34bcb30baf786b630e9456b9cd66ef467633b
Eric Demicco <edemicco@gmail.com>
2013-03-23 02:28:48 -0400

    Files for tumblr

---
+++ app/modules/tumblr.js
@@ -0,0 +1,70 @@
+define([
+    "app"
+], function( App ) {
+    
+    var Tumblr;
+
+    Tumblr = App.module();
+
+    Tumblr.Model = Backbone.Model.extend({
+        // override sync as Tumblr uses JSONP
+        sync: function(method, model, options) {
+            var params = _.extend({
+                type: 'GET',
+                dataType: 'jsonp',
+                url: this.url(),
+                processData: false
+            }, options);
+
+            return $.ajax(params);
+        },
+
+        // we're only interested in the latest post
+        parse: function(response) {
+            return response.posts[0];
+        },
+
+        url: function() {
+            return "http://blog.reinventionstories.org/api/read/json?num=1";
+        }
+    });
+
+    Tumblr.View = Backbone.LayoutView.extend({
+        template: "tumblr",
+        className: "tumblr-news",
+        events: {
+            "click .close-news" : "hide"
+        },
+        initialize: function() {
+            this.model = new Tumblr.Model();
+
+            this.model.fetch();
+
+            this.model.on("change", this.render, this);
+        },
+        serialize: function() {
+            var months, dateObj = new Date( this.model.get("date") );
+
+            months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+
+            if ( this.model.has("regular-title") ) {
+                return {
+                    title: this.model.get("regular-title"),
+                    url: this.model.get("url-with-slug"),
+                    date: months[ dateObj.getMonth() ] + " " + dateObj.getDate() + ", " + dateObj.getFullYear()
+                };
+            } else {
+                return {
+                    title: "",
+                    url: "",
+                    date: ""
+                };
+            }
+        },
+        hide: function() {
+            this.remove();
+        }
+    });
+
+    return Tumblr;
+});
