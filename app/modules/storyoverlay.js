d1769f93d81d5625a8700472ffca2e3c7975b297
Eric Demicco <edemicco@gmail.com>
2013-03-28 17:57:33 -0400

    Story overlay view improved.

--- app/modules/storyoverlay.js
+++ app/modules/storyoverlay.js
@@ -12,11 +12,12 @@ define([
         className: "story-player",
 
         events: {
-            "click .player-close" : "closeStory"
+            "click .player-close" : "closeStory",
+            "click .fullscreen" : "goFullScreen",
+            "click .share" : "showShareIcons"
         },
 
         afterRender: function() {
-            // TODO: move events bindings to "events"
             this.player = new Zeega.player({
                 controls: {
                     arrows: true,
@@ -36,21 +37,10 @@ define([
                 $(".share-email").attr("href", "mailto:friend@example.com?subject=Check out this story on Reinvention Stories!&body=http://reinventionstories.org/%23story/" + this.id );
             }.bind(this) );
 
-            $(".fullscreen").on( "click", function(){
-                var $playerElem = $(".ZEEGA-player").get(0);
-
-                if ($playerElem.requestFullscreen) {
-                  $playerElem.requestFullscreen();
-                } else if ($playerElem.mozRequestFullScreen) {
-                  $playerElem.mozRequestFullScreen();
-                } else if ($playerElem.webkitRequestFullscreen) {
-                  $playerElem.webkitRequestFullscreen();
-                }
-            });
-
-            $(".share").on( "click", function(){
-                $(this).find(".icons").toggle();
-            });
+            // kill player when App's kill_player event is triggered (when switching to another view)
+            App.on( "kill_player", function() {
+                this.player.destroy();
+            }.bind(this) );
 
             $(".story-player").fadeIn();
 
@@ -59,6 +49,22 @@ define([
         closeStory: function() {
             this.player.destroy();
             this.remove();
+        },
+
+        goFullScreen: function() {
+            var $playerElem = this.$(".ZEEGA-player").get(0);
+
+            if ($playerElem.requestFullscreen) {
+              $playerElem.requestFullscreen();
+            } else if ($playerElem.mozRequestFullScreen) {
+              $playerElem.mozRequestFullScreen();
+            } else if ($playerElem.webkitRequestFullscreen) {
+              $playerElem.webkitRequestFullscreen();
+            }
+        },
+
+        showShareIcons: function(event) {
+            this.$(".share .icons").toggle();
         }
 
     });
