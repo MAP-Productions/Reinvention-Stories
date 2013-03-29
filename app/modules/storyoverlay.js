039f61c2d650cc4fb40561617be7a5b634bcf658
Eric Demicco <edemicco@gmail.com>
2013-03-29 02:55:32 -0400

    Fix bug in story overlay w/ player getting inserted twice. Timeline display for video-based Zeegas.

--- app/modules/storyoverlay.js
+++ app/modules/storyoverlay.js
@@ -21,28 +21,34 @@ define([
         },
 
         afterRender: function() {
-            this.player = new Zeega.player({
-                controls: false,
-                autoplay: true,
-                target: "#story-zeega-player",
-                url: "/zeegaapi/items/" + this.id,
-                windowRatio: 16/9
-            });
 
-            // update share URLs
-            this.player.on("sequence_enter", function(info) {
-                $(".share-twitter").attr("href", "https://twitter.com/intent/tweet?original_referer=http://reinventionstories.org/%23story/" + this.id + "&text=Reinvention%20Stories%3A%20" + this.player.project.get( "title" ) + "&url=http://reinventionstories.org/%23story/" + this.id );
-                $(".share-fb").attr("href", "http://www.facebook.com/sharer.php?u=http://reinventionstories.org/%23story/" + this.id );
-                $(".share-email").attr("href", "mailto:friend@example.com?subject=Check out this story on Reinvention Stories!&body=http://reinventionstories.org/%23story/" + this.id );
-            }.bind(this) );
+            if ( !this.player ) {
+                this.player = new Zeega.player({
+                    controls: false,
+                    autoplay: true,
+                    target: "#story-zeega-player",
+                    url: "/zeegaapi/items/" + this.id,
+                    windowRatio: 16/9
+                });
 
-            // update arrows
-            this.player.on("frame_play", this.updateArrowState, this);
+                // update share URLs
+                this.player.on("sequence_enter", function(info) {
+                    $(".share-twitter").attr("href", "https://twitter.com/intent/tweet?original_referer=http://reinventionstories.org/%23story/" + this.id + "&text=Reinvention%20Stories%3A%20" + this.player.project.get( "title" ) + "&url=http://reinventionstories.org/%23story/" + this.id );
+                    $(".share-fb").attr("href", "http://www.facebook.com/sharer.php?u=http://reinventionstories.org/%23story/" + this.id );
+                    $(".share-email").attr("href", "mailto:friend@example.com?subject=Check out this story on Reinvention Stories!&body=http://reinventionstories.org/%23story/" + this.id );
+                }.bind(this) );
 
-            // kill player when App's kill_player event is triggered (when switching to another view)
-            App.on( "kill_player", function() {
-                this.player.destroy();
-            }.bind(this) );
+                // bind progress bar for playback time display
+                this.player.on('media_timeupdate', this.updateTimeline, this );
+
+                // update arrows
+                this.player.on("frame_play", this.updateArrowState, this);
+
+                // kill player when App's kill_player event is triggered (when switching to another view)
+                App.on( "kill_player", function() {
+                    this.player.destroy();
+                }.bind(this) );
+            }
 
         },
 
@@ -103,6 +109,33 @@ define([
 
         disableArrow: function(className) {
             this.$("."+ className).addClass("disabled");
+        },
+
+        updateTimeline: function( info ) {
+          var elapsed, duration;
+
+          if ( info.cue_in && info.cue_out ) {
+            duration = info.cue_out - info.cue_in;
+            elapsed = info.current_time - info.cue_in;
+          } else if ( info.cue_out ) {
+            duration = info.cue_out;
+            elapsed = info.current_time;
+          } else {
+            duration = info.duration;
+            elapsed = info.current_time;
+          }
+
+          this.$('.timeline-indicator').css('width', (elapsed / duration * 100) + "%");
+
+        },
+
+        _convertTime: function( seconds ) {
+            var m = Math.floor( seconds / 60 );
+            var s = Math.floor( seconds % 60 );
+            if ( s < 10 ) {
+                s = "0" + s;
+            }
+            return m + ":" + s;
         }
 
     });
